2326a47a5e19108ebd3743624c2f53f72cac5d44
Michał Szwejk <mszwejk1@gmail.com>
2026-01-14 17:22:46 +0100

    finish

--- src/builder.py
+++ src/builder.py
@@ -24,25 +24,3 @@ class FeatureBuilder(BaseEstimator, TransformerMixin):
             sessions_features, left_on="id", right_on="listing_id", how="left"
         )
         return features.drop(columns=["listing_id", "id"])
-
-
-def generate_logs(listings, sessions, n):
-    target = target_transforms.transform_pipeline(listings["price"])
-    builder = FeatureBuilder(sessions)
-    X = builder.fit_transform(listings)
-    X["price"] = target
-    indices = X.index.to_list()
-    i = 0
-    while i != n:
-        idx = np.random.choice(indices)
-        x = X.iloc[idx]
-        if x.isna().sum() != 0:
-            continue
-        _ = predict_price(PredictionData(**x.to_dict()))
-        i += 1
-
-
-if __name__ == "__main__":
-    listings = pd.read_csv("./data/data_new/listings.csv")
-    sessions = pd.read_csv("./data/data_new/sessions.csv")
-    generate_logs(listings, sessions, 1965)
